3e81d8250d6ceacf0fb3992923691a2fdbca072b
FelipeSantos1423 <felipsantonios@gmail.com>
2026-05-19 21:08:04 -0300

    NavBar iniciada, logo e nome trocados

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,14 @@
+import Navbar from "../components/Navbar"
+import "./globals.css"
+
 export default function Home() {
   return (
-    <div className="bg-dark text-white min-h-screen p-10">
-      <h1 className="text-4xl font-bold">
-        Catálogo de Filmes
-      </h1>
+    <div>
+      <Navbar />
+
+      <main className="bg-white min-h-screen p-10 text-black">
+        <h1>MAIN</h1>
+      </main>
     </div>
   )
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,19 @@
+import '../app/globals.css'
 export default function Navbar() {
   return (
-    <nav className="bg-dark text-white p-4">
-      <h1>Catálogo de Filmes</h1>
-    </nav>
+    
+   <nav className="flex justify-between items-center p-6 w-full"
+  style={{ backgroundColor: "var(--red-color)" }}>
+
+  <h1 className="text-white text-2xl font-bold">
+    Catálogo de Filmes
+  </h1>
+
+  <div className="flex gap-4">
+    <input type="text" placeholder="Buscar filmes..."  className="bg-gray-800 text-white placeholder:text-gray-500 border border-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-500 rounded-md shadow-md p-2"/>
+
+    <button className="bg-white text-red-500 hover:bg-gray-200 rounded-md shadow-md p-2"> Buscar </button>
+  </div>
+</nav>
   )
 }
